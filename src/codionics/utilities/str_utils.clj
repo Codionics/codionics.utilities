84d9e016c131f8a79848acc9ae8c3ccf4a4fd028
Manoj Waikar <mmwaikar@gmail.com>
2011-10-18 16:27:19 +0530

    Add a without function to remove unwanted characters from a string.

--- src/codionics/utilities/str_utils.clj
+++ src/codionics/utilities/str_utils.clj
@@ -1,4 +1,6 @@
-(ns codionics.utilities.str-utils)
+(ns codionics.utilities.str-utils
+  (:refer-clojure :exclude [replace])
+  (:use [clojure.string :only [replace]]))
 
 (defn- betw [s start start-index end-index]
   {:pre [(and (>= start-index 0) (< start-index (count s)))]}
@@ -14,3 +16,9 @@
     (if (or (= end-index -1) (> end-index len))
       (betw s start start-index len)
       (betw s start start-index end-index))))
+
+(defn without [s & args]
+  "Replaces each of the args with an empty string (i.e. \"\") and returns the resulting string."
+  (if (empty? args)
+     s
+     (recur (replace s (str (first args)) "") (rest args))))
--- test/codionics/utilities/test/str_utils.clj
+++ test/codionics/utilities/test/str_utils.clj
@@ -1,12 +1,17 @@
 (ns codionics.utilities.test.str-utils
-  (:use [codionics.utilities.str-utils :only [between]]
+  (:use [codionics.utilities.str-utils :only [between without]]
         [midje.sweet]))
 
 ;; Globals
 (def *test-string* "and miles to go before I sleep")
+(def *test-string-with-punctuations* "and miles\n to, go before, I sleep\n")
 
 ;; Tests
 
 (fact (between *test-string* "miles" "I") => (just " to go before "))
 (fact (between *test-string* "miles" "slept") => (just " to go before I sleep"))
 (fact (between *test-string* "miley" "I") => (throws AssertionError (contains "Assert failed:")))
+
+(fact (without (between *test-string-with-punctuations* "miles" "slept") "\n") => (just " to, go before, I sleep"))
+(fact (without (between *test-string-with-punctuations* "miles" "slept") "\n" ",") => (just " to go before I sleep"))
+(fact (without (between *test-string-with-punctuations* "miles" "slept") "\n" " " ",") => (just "togobeforeIsleep"))
